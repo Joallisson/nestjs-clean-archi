b810e25d203f127526fdab1fe9c5894fa3a1b61a
joallisson <joallissonnc@gmail.com>
2024-01-02 11:37:05 -0300

    40. Testes de Integração da entidade User com a Validação - atualização usuário

--- src/users/domain/entities/__tests__/integration/user.entity.int-spec.ts
+++ src/users/domain/entities/__tests__/integration/user.entity.int-spec.ts
@@ -111,4 +111,26 @@ describe('UserEntity integration tests', () => {
     })
   })
 
+  describe('Update method', () => {
+    it('Should throw an error when update a user with invalid name', () => {
+      const entity = new UserEntity(UserDataBuilder({}))
+
+      expect(() => entity.update(null)).toThrowError(EntityValidationError)
+      expect(() => entity.update('')).toThrowError(EntityValidationError)
+      expect(() => entity.update(10 as any)).toThrowError(EntityValidationError)
+      expect(() => entity.update('a'.repeat(256))).toThrowError(EntityValidationError)
+    })
+
+    it('Should a value user', () => {
+      expect.assertions(0)
+
+      const props: UserProps = {
+        ...UserDataBuilder({}),
+      }
+      const entity = new UserEntity(props)
+      entity.update('other name')
+
+    })
+  })
+
 })
