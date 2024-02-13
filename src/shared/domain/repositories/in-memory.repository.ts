1fd5f95fc682bc6cfa5cad9638d25179b0ae0254
Joallisson <joallisson@gmail.com>
2024-02-13 12:17:06 -0300

    44. Criação da abstração para o repositório InMemoryRepository - parte 1

---
+++ src/shared/domain/repositories/in-memory.repository.ts
@@ -0,0 +1,31 @@
+import { Entity } from '../entities/entity'
+import { RepositoryInterface } from './respository-contracts'
+
+export abstract class InMemoryRepository<E extends Entity>
+  implements RepositoryInterface<E>
+{
+  items: E[] = []
+
+  async insert(entity: E): Promise<void> {
+    this.items.push(...this.items, entity)
+  }
+  async findById(id: string): Promise<E> {
+    const _id = `${id}`
+    const entity = this.items.find(item => item.id === _id)
+
+    if (!entity) {
+      throw new Error('Entity not found')
+    }
+
+    return entity
+  }
+  async findAll(): Promise<E[]> {
+    return this.items
+  }
+  update(entity: E): Promise<void> {
+    throw new Error('Method not implemented.')
+  }
+  delete(id: string): Promise<void> {
+    throw new Error('Method not implemented.')
+  }
+}
