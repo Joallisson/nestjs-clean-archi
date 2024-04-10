9a93378f2b3d7bf5febdd00687bbbc0f0d0bb3d6
Joallisson <joallisson@gmail.com>
2024-04-10 18:56:49 -0300

    71. Refatoração do repositório de usuários - parte 2

--- src/users/infrastructure/database/in-memory/repositories/user-in-memory.repository.ts
+++ src/users/infrastructure/database/in-memory/repositories/user-in-memory.repository.ts
@@ -6,8 +6,10 @@ import { UserRepository } from '@/users/domain/repositories/user.repository'
 
 export class UserInMemoryRepository
   extends InMemorySearchableRepository<UserEntity>
-  implements UserRepository
+  implements UserRepository.Repository
 {
+  sortableFields: string[] = ['name', 'createdAt']
+
   async findByEmail(email: string): Promise<UserEntity> {
     const entity = this.items.find(item => item.email === email)
     if (!entity) {
@@ -22,5 +24,29 @@ export class UserInMemoryRepository
     if (entity) {
       throw new ConflictError('Email address already used')
     }
+  }
+
+  protected async applyFilter(
+    items: UserEntity[],
+    filter: string
+  ): Promise<UserEntity[]> {
+
+    if(!filter){
+      return items
+    }
+
+    return items.filter(item => {
+      return item.props.name.toLowerCase().includes(filter.toLowerCase())
+    })
+  }
+
+  protected async applySort(
+    items: UserEntity[],
+    sort: string,
+    sortDir: string
+  ): Promise<UserEntity[]> {
+      return !sort
+      ? super.applySort(items, 'createdAt', 'desc')
+      : super.applySort(items, sort, sortDir)
   }
 }
