dc73587a5b0b9926af7e12cb1974926d822ca577
joallisson <joallissonnc@gmail.com>
2024-04-30 10:06:20 -0300

    143. Implementação da classe UserPresenter

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,18 @@
-import { NestFactory } from '@nestjs/core';
+import { NestFactory, Reflector } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 import {
   FastifyAdapter,
   NestFastifyApplication,
 } from '@nestjs/platform-fastify';
+import { ClassSerializerInterceptor } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestFastifyApplication>(AppModule, new FastifyAdapter());
   await app.listen(3000, '0.0.0.0');
+
+  app.useGlobalInterceptors(new ClassSerializerInterceptor(app.get(Reflector)))
 }
+
+
 bootstrap();
