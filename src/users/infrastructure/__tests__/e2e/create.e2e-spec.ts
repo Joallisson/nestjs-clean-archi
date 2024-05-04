6af444c2b92e92df5f479562274f6fc3f7cf6d7f
Joallisson <joallisson@gmail.com>
2024-05-04 15:37:54 -0300

    156. Implementação do interceptor WrapperData - parte 1

--- src/users/infrastructure/__tests__/e2e/create.e2e-spec.ts
+++ src/users/infrastructure/__tests__/e2e/create.e2e-spec.ts
@@ -10,6 +10,7 @@ import { PrismaClient } from "@prisma/client";
 import request from 'supertest'
 import { UsersController } from "../../users.controller";
 import { instanceToPlain } from "class-transformer";
+import { applyGlobalConfig } from "@/global-config";
 
 describe('UsersController e2e tests', () => {
   let app: INestApplication
@@ -28,6 +29,7 @@ describe('UsersController e2e tests', () => {
       ]
     }).compile()
     app = module.createNestApplication()
+    applyGlobalConfig(app)
     await app.init()
     repository = module.get<UserRepository.Repository>('UserRepository')
   })
