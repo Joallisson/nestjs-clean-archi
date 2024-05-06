8cdc00bef08dae31f9e708318dea31ede3afceac
joallisson <joallissonnc@gmail.com>
2024-05-06 09:08:48 -0300

    160. Validação dos dados do cliente com o ValidationPipe

--- src/users/infrastructure/dtos/update-user.dto.ts
+++ src/users/infrastructure/dtos/update-user.dto.ts
@@ -1,6 +1,9 @@
 import { UpdateUserUseCase } from "@/users/application/usecases/update-user.usecase";
+import { IsNotEmpty, IsString } from "class-validator";
 
 
 export class UpdateUserDto implements Omit<UpdateUserUseCase.Input, 'id'> {
+  @IsString()
+  @IsNotEmpty()
   name: string;
 }
