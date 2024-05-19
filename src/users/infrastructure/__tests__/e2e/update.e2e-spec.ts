7cd50bd83e12c1ab3e46524ad3dcf12bf648815b
Joallisson <joallisson@gmail.com>
2024-05-19 15:12:26 -0300

    189. Refatoração dos testes e2e do método update de UsersController

--- src/users/infrastructure/__tests__/e2e/update.e2e-spec.ts
+++ src/users/infrastructure/__tests__/e2e/update.e2e-spec.ts
@@ -13,6 +13,8 @@ import { applyGlobalConfig } from "@/global-config";
 import { UserEntity } from "@/users/domain/entities/user.entity";
 import { UserDataBuilder } from "@/users/domain/testing/helpers/user-data-builder";
 import { UpdateUserDto } from "../../dtos/update-user.dto";
+import { HashProvider } from "@/shared/application/providers/hash-provider";
+import { BcryptjsHashProvider } from "../../providers/hash-provider/bcryptjs-hash-provider";
 
 describe('UsersController e2e tests', () => {
   let app: INestApplication
@@ -21,6 +23,9 @@ describe('UsersController e2e tests', () => {
   let updateUserDto: UpdateUserDto
   const prismaService = new PrismaClient()
   let entity: UserEntity
+  let hashProvider: HashProvider
+  let hashPassword: string
+  let accessToken: string
 
   beforeAll(async () => {
     setuptPrismaTests()
@@ -35,6 +40,8 @@ describe('UsersController e2e tests', () => {
     applyGlobalConfig(app)
     await app.init()
     repository = module.get<UserRepository.Repository>('UserRepository')
+    hashProvider = new BcryptjsHashProvider()
+    hashPassword = await hashProvider.generateHash('1234')
   })
 
   beforeEach(async () => {
@@ -42,59 +49,83 @@ describe('UsersController e2e tests', () => {
       name: 'test name',
     }
     await prismaService.user.deleteMany()
-    entity = new UserEntity(UserDataBuilder({}))
+    entity = new UserEntity(UserDataBuilder({
+      email: 'a@a.com',
+      password: hashPassword,
+    }))
     await repository.insert(entity)
+
+    const loginResponse = await request(app.getHttpServer())
+      .post('/users/login')
+      .send({email: 'a@a.com', password: '1234'})
+      .expect(200)
+
+    accessToken = loginResponse.body.accessToken
   });
 
-    describe('PUT /users/:id', () => {
-      it('should update a user', async () => {
-        updateUserDto.name = 'test name'
-        const res = await request(app.getHttpServer())
-        .put(`/users/${entity._id}`)
-        .send(updateUserDto)
-        .expect(200)
+  describe('PUT /users/:id', () => {
+    it('should update a user', async () => {
+      updateUserDto.name = 'test name'
+      const res = await request(app.getHttpServer())
+      .put(`/users/${entity._id}`)
+      .set('Authorization', `Bearer ${accessToken}`)
+      .send(updateUserDto)
+      .expect(200)
 
-        const user = await repository.findById(entity._id)
-        const presenter = UsersController.userToResponse(user.toJSON())
-        const serialized = instanceToPlain(presenter)
-        expect(res.body.data).toStrictEqual(serialized)
-      })
+      const user = await repository.findById(entity._id)
+      const presenter = UsersController.userToResponse(user.toJSON())
+      const serialized = instanceToPlain(presenter)
+      expect(res.body.data).toStrictEqual(serialized)
+    })
 
-      it('should return a error 422 code when the request body is invalid', async () => {
-        const res = await request(app.getHttpServer())
-        .put(`/users/${entity._id}`)
-        .send({})
-        .expect(422)
-        expect(res.body.error).toBe('Unprocessable Entity')
-        expect(res.body.message).toEqual([
-          'name should not be empty',
-          'name must be a string',
-        ])
-      })
+    it('should return a error 422 code when the request body is invalid', async () => {
+      const res = await request(app.getHttpServer())
+      .put(`/users/${entity._id}`)
+      .set('Authorization', `Bearer ${accessToken}`)
+      .send({})
+      .expect(422)
+      expect(res.body.error).toBe('Unprocessable Entity')
+      expect(res.body.message).toEqual([
+        'name should not be empty',
+        'name must be a string',
+      ])
+    })
 
-      it('should return a error 422 code when the name field is invalid', async () => {
-        delete updateUserDto.name
-        const res = await request(app.getHttpServer())
-        .put(`/users/${entity._id}`)
-        .send(updateUserDto)
-        .expect(422)
-        expect(res.body.error).toBe('Unprocessable Entity')
-        expect(res.body.message).toEqual([
-          'name should not be empty',
-          'name must be a string',
-        ])
-      })
+    it('should return a error 422 code when the name field is invalid', async () => {
+      delete updateUserDto.name
+      const res = await request(app.getHttpServer())
+      .put(`/users/${entity._id}`)
+      .set('Authorization', `Bearer ${accessToken}`)
+      .send(updateUserDto)
+      .expect(422)
+      expect(res.body.error).toBe('Unprocessable Entity')
+      expect(res.body.message).toEqual([
+        'name should not be empty',
+        'name must be a string',
+      ])
+    })
 
-      it('should return a error 404 code when throw NotFoundError with invalid id', async () => {
-        const res = await request(app.getHttpServer())
-        .put(`/users/fakeId`)
-        .send(updateUserDto)
-        .expect(404)
-        .expect({
-          statusCode: 404,
-          error: 'Not Found',
-          message: 'UserModel not found using ID fakeId'
-        })
+    it('should return a error 404 code when throw NotFoundError with invalid id', async () => {
+      const res = await request(app.getHttpServer())
+      .put(`/users/fakeId`)
+      .set('Authorization', `Bearer ${accessToken}`)
+      .send(updateUserDto)
+      .expect(404)
+      .expect({
+        statusCode: 404,
+        error: 'Not Found',
+        message: 'UserModel not found using ID fakeId'
       })
     })
+
+    it('should return a error 401 code when the request is not authorized', async () => {
+      await request(app.getHttpServer())
+      .put(`/users/fakeId`)
+      .expect(401)
+      .expect({
+        statusCode: 401,
+        message: 'Unauthorized'
+      })
+    })
+  })
   })
