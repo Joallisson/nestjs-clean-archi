f7dbd8e8ea3e1ef05879cc1f07a8006851caba90
Joallisson <joallisson@gmail.com>
2024-05-20 12:50:17 -0300

    194. Configuração do Swagger nas classes DTOs

--- src/users/infrastructure/dtos/update-password.dto.ts
+++ src/users/infrastructure/dtos/update-password.dto.ts
@@ -1,12 +1,15 @@
 import { UpdatePasswordUseCase } from "@/users/application/usecases/update-password.usecase";
+import { ApiProperty } from "@nestjs/swagger";
 import { IsNotEmpty, IsString } from "class-validator";
 
 
 export class UpdatePasswordDto implements Omit<UpdatePasswordUseCase.Input, 'id'> {
+  @ApiProperty({ description: 'Senha do usuário' })
   @IsString()
   @IsNotEmpty()
   password: string;
 
+  @ApiProperty({ description: 'Senha antiga do usuário' })
   @IsString()
   @IsNotEmpty()
   oldPassword: string;
